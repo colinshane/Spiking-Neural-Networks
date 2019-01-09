e1cf93b63f0f8f4e289fc2727ea71e8464ee7683
nmasse <masse@uchicago.edu>
2019-01-09 09:58:09 -0600

    Adding custom gradient trick.

--- model.py
+++ model.py
@@ -76,6 +76,8 @@ class Model:
 
 		# Set up output record
 		self.output = []
+		self.syn_x = []
+		self.syn_u = []
 
 		y = 0.
 		for t in range(par['num_time_steps']):
@@ -83,11 +85,13 @@ class Model:
 				raise Exception('Rate cell not yet implemented.')
 			elif par['cell_type'] == 'adex':
 				spike, state, adapt, syn_x, syn_u = self.AdEx_cell(self.h_out[-1], self.h[-1], adapt, self.input_data[t], syn_x, syn_u)
-				y = 0.98*y + 0.02*(spike @ self.var_dict['W_out'] + 0.*self.var_dict['b_out'])
+				y = 0.95*y + 0.05*(spike @ self.var_dict['W_out'] + self.var_dict['b_out'])
 
 				self.h_out.append(spike)
 				self.h.append(state)
 				self.output.append(y)
+				self.syn_x.append(syn_x)
+				self.syn_u.append(syn_u)
 
 			elif par['cell_type'] == 'lif':
 				spike, state, adapt, syn_x, syn_u = self.LIF_cell(self.h_out[-1], self.h[-1], adapt, self.input_data[t], syn_x, syn_u)
@@ -101,20 +105,22 @@ class Model:
 		self.output = tf.stack(self.output, axis=0)
 		self.h = tf.stack(self.h, axis=0)
 		self.h_out = tf.stack(self.h_out, axis=0)
+		self.syn_x = tf.stack(self.syn_x, axis=0)
+		self.syn_u = tf.stack(self.syn_u, axis=0)
 
 
-	def synaptic_plasticity(self, h, syn_x, syn_u):
+	def synaptic_plasticity(self, spike, syn_x, syn_u):
 
 		if par['use_stp']:
-			syn_x += par['alpha_std']*(1-syn_x) - par['stp_mod']*syn_u*syn_x*h
-			syn_u += par['alpha_stf']*(par['U']-syn_u) + par['stp_mod']*par['U']*(1-syn_u)*h
+			syn_x += par['alpha_std']*(1-syn_x) - par['stp_mod']*syn_u*syn_x*spike
+			syn_u += par['alpha_stf']*(par['U']-syn_u) + par['stp_mod']*par['U']*(1-syn_u)*spike
 			syn_x = tf.minimum(1., tf.nn.relu(syn_x))
 			syn_u = tf.minimum(1., tf.nn.relu(syn_u))
-			h_post = syn_u*syn_x*h
+			spike_post = syn_u*syn_x*spike
 		else:
-			h_post = h
+			spike_post = spike
 
-		return h_post, syn_x, syn_u
+		return spike_post, syn_x, syn_u
 
 
 	def run_adex(self, V, w, I):
@@ -133,16 +139,25 @@ class Model:
 		w_next = w + (c['dt']/c['tau'])*(term1-term2)
 
 		# Check potential threshold for new spikes
+		"""
 		@tf.custom_gradient
 		def do_spike(V):
-			def grad(dy): return -0.3*tf.nn.relu(1-tf.abs((V-c['Vth'])/(0.02 + c['Vth'])))
+			def grad(dy):
+				return  -0.3*tf.nn.relu(1-tf.abs((V-c['Vth'])/(0.02 + c['Vth'])))
 			return tf.cast(V >= c['Vth'], tf.float32), grad
+		"""
+		def do_spike(V):
+			spike = tf.cast(V >= c['Vth'], tf.float32)
+			spike_close  = tf.stop_gradient(tf.cast(V >= c['Vth']-0.02, tf.float32))
+			spike_grad = 0.3*(V - 0.02*0.5*((V-c['Vth'])/0.02)**2)*spike_close
+			return spike + spike_grad - tf.stop_gradient(spike_grad)
 
 		spike = do_spike(tf.minimum(c['Vth']+1e-9, V_next))
 
 		# Apply spike to membrane and adaptation
-		V_new = spike*c['V_r']        + (1-spike)*V_next
-		w_new = spike*(w_next+c['b']) + (1-spike)*w_next
+		spike_stop = tf.stop_gradient(spike)
+		V_new = spike_stop*c['V_r']        + (1-spike_stop)*V_next
+		w_new = spike_stop*(w_next+c['b']) + (1-spike_stop)*w_next
 
 		return V_new, w_new, spike
 
@@ -178,6 +193,7 @@ class Model:
 	def AdEx_cell(self, spike, V, w, rnn_input, syn_x, syn_u):
 
 		# Apply synaptic plasticity
+		#spike_stop = tf.stop_gradient(spike)
 		spike_post, syn_x, syn_u = self.synaptic_plasticity(spike, syn_x, syn_u)
 
 		I = rnn_input @ self.var_dict['W_in'] + spike_post @ self.W_rnn_effective + self.var_dict['b_rnn']
@@ -196,8 +212,10 @@ class Model:
 		self.task_loss = tf.reduce_mean(self.time_mask * \
 				tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.output, labels=self.target_data))
 
+		self.spike_loss = 0.*tf.reduce_mean(tf.nn.relu(self.h_out))
+
 		# Compute gradients
-		self.train = adam_optimizer.compute_gradients(self.task_loss)
+		self.train = adam_optimizer.compute_gradients(self.task_loss + self.spike_loss)
 
 
 def main(gpu_id=None):
@@ -242,9 +260,9 @@ def main(gpu_id=None):
 			feed_dict = {x:trial_info['neural_input'], y:trial_info['desired_output'], m:trial_info['train_mask']}
 
 			# Run the model
-			_, task_loss, output, state, spike = \
+			_, task_loss, output, state, spike, syn_x, syn_u = \
 				sess.run([model.train, model.task_loss, \
-				model.output, model.h, model.h_out], feed_dict=feed_dict)
+				model.output, model.h, model.h_out, model.syn_x, model.syn_u], feed_dict=feed_dict)
 
 			# Display network performance
 			if i%20 == 0:
@@ -257,24 +275,27 @@ def main(gpu_id=None):
 				data_record['spiking'].append(spiking)
 
 				trials = 4
-				fig, ax = plt.subplots(5,trials, figsize=[12,8])
+				fig, ax = plt.subplots(6,trials, figsize=[12,8])
 				for b in range(trials):
 					ax[0,b].imshow(trial_info['neural_input'][:,b,:].T, aspect='auto')
 					ax[1,b].imshow(trial_info['desired_output'][:,b,:].T, aspect='auto')
 					ax[2,b].imshow(output[:,b,:].T, aspect='auto')
 					ax[3,b].imshow(state[:,b,:].T, aspect='auto')
 					ax[4,b].imshow(spike[:,b,:].T, aspect='auto')
+					ax[5,b].imshow((syn_x[:,b,:]*syn_y[:,b,:]).T, aspect='auto')
 
 				ax[0,0].set_ylabel('Network input')
 				ax[1,0].set_ylabel('Expected Output')
 				ax[2,0].set_ylabel('Network Output')
 				ax[3,0].set_ylabel('Membrane Voltage')
 				ax[4,0].set_ylabel('Spike Output')
+				ax[4,0].set_ylabel('Synaptic Eff.')
 				ax[4,0].set_xlabel('Time')
 
 				plt.savefig('./savedir/iter{}_outputs.png'.format(i))
 				plt.clf()
 				plt.close()
+
 
 				pickle.dump(data_record, open(par['savedir'] + par['save_fn'] + '.pkl', 'wb'))
 
